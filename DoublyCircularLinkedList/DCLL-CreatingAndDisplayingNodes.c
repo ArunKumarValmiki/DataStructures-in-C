5696b3023e69cb8943cf97bf4d41408ed080fee1
ArunKumarValmiki <valmikiarunkumar5@gmail.com>
2024-12-28 23:01:18 +0530

    Create DCLL-CreatingAndDisplayingNodes.c

---
+++ DoublyCircularLinkedList/DCLL-CreatingAndDisplayingNodes.c
@@ -0,0 +1,73 @@
+# include <stdio.h> 
+# include <stdlib.h> 
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+struct node *head = NULL;
+
+void createlist();
+void display(); 
+
+void createlist()
+{
+    int n;
+    printf("Enter the number of nodes: ");
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        struct node *newnode, *temp;
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter the data value for node %d: ",i+1);
+        scanf("%d",&newnode -> data);
+        newnode -> prev = NULL;
+        newnode -> next = NULL; 
+        
+        if (head == NULL)
+        {
+            head = newnode;
+            newnode -> next = newnode;
+            newnode -> prev = newnode;
+            temp = head;
+        }
+        else
+        {
+            temp -> next = newnode;
+            newnode -> prev = temp;
+            newnode -> next = head;
+            head -> prev = newnode;
+            temp = newnode;
+        }
+    }
+}
+
+void display()
+{
+    struct node *temp;
+    int count = 0;
+    
+    if (head == NULL)
+    {
+        printf("Empty list");
+    }
+    else
+    {
+        temp = head;
+        do {
+            printf("%d ",temp->data);
+            temp = temp -> next;
+            count++;
+        } while (temp != head);
+        printf("\nThe number of nodes are %d",count);
+    }
+}
+
+int main()
+{
+    createlist();
+    display();
+}
